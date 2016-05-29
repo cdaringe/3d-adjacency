35c3ce0dae9777bc7da3ead700eafb9fe6de93cd
cdaringe <christopher.dieringer@gmail.com>
2016-05-29 02:32:44 +0000

    use for-loops vs forEach

--- src/index.js
+++ src/index.js
@@ -3,25 +3,29 @@
 const Cluster = require('./cluster')
 const Mass = require('./mass')
 
-function find(xArr) {
+function find(arr) {
   var clusters = []
   var clust
-  xArr.forEach((yArr, x) => {
-    yArr.forEach((zArr, y) => {
-      zArr.forEach((value, z) => {
-        // console.log(x + ' ' + y + ' ' + z + ' - ' + value)
-        if (!Mass.isMasslike(value)) return
-        if (Mass.isMass(value)) return
-        clust = new Cluster({
-          domain: xArr,
-          root: new Mass({ domain: xArr, x: x, y: y, z: z, value: value }),
-        })
-        xArr[x][y][z] = clust.root
-        clust.clusterify()
-        clusters.push(clust.serialize())
-      })
-    })
-  })
+  var value
+  for (var x = 0; x < arr.length; ++x) {
+    for (var y = 0; y < arr[0].length; ++y) {
+      for (var z = 0; z < arr[0][0].length; ++z) {
+        value = arr[x][y][z]
+        if (!Mass.isMasslike(value) || Mass.isMass(value)) {
+          // pass if content is non-massy or we've already
+          // massified the cell
+        } else {
+          clust = new Cluster({
+            domain: arr,
+            root: new Mass({ domain: arr, x, y, z, value: value }),
+          })
+          arr[x][y][z] = clust.root
+          clust.clusterify()
+          clusters.push(clust.serialize())
+        }
+      }
+    }
+  }
   return clusters
 }
 
